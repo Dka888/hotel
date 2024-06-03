b694da3570db605f5b862b4d372b10271bffe724
Dymitr <dymitrkosow@gmail.com>
2024-06-03 16:52:20 +0200

    testComponent

--- src/components/__tests__/MenuHeader.spec.ts
+++ src/components/__tests__/MenuHeader.spec.ts
@@ -1,6 +1,38 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import MenuHeader from '../MenuHeader.vue'
+import router from '@/router'
 
-describe()
+describe('MenuHeader Component', () => {
+  it('should render menu links', async () => {
+    const wrapper = mount(MenuHeader, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await router.isReady()
+
+    expect(wrapper.findAll('.menu__item').length).toBe(4)
+    expect(wrapper.find('.menu__item:nth-child(1)').text()).toBe('Home')
+    expect(wrapper.find('.menu__item:nth-child(2)').text()).toBe('About')
+    expect(wrapper.find('.menu__item:nth-child(3)').text()).toBe('Rooms')
+    expect(wrapper.find('.menu__item:nth-child(4)').text()).toBe('Services')
+  })
+
+  it('should toggle mobile menu', async () => {
+    const wrapper = mount(MenuHeader, {
+      global: {
+        plugins: [router]
+      }
+    })
+
+    await router.isReady()
+
+    expect(wrapper.find('.menuPhone__items').exists()).toBe(false)
+    await wrapper.find('.menuPhone').trigger('click')
+    expect(wrapper.find('.menuPhone__items').exists()).toBe(true)
+    await wrapper.find('.closeMenu').trigger('click')
+    expect(wrapper.find('.menuPhone__items').exists()).toBe(false)
+  })
+})
